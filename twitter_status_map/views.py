1a53d86a11922247fe670c9dfe88a882122089e5
olavurmortensen <olavurmortensen@gmail.com>
2012-11-25 17:08:44 +0100

    Added a search box and coupled it with the map view.

--- twitter_status_map/views.py
+++ twitter_status_map/views.py
@@ -8,7 +8,16 @@ from twitter_status_map.models import Map
 
 
 def index(request):
+    errors=[]
+    if 'q' in request.GET:
+        address=request.GET['q']
+        if not address:
+            errors.append('You entered an empty address.')
+            address='Denmark, Anker Engelundsvej 1'
+    else:
+        address='Denmark, Anker Engelundsvej 1'
+    
     cache_timeout = 900
     first_map = Map.objects.all()[0]
-    context = {'first_map': first_map, 'cache_timeout': cache_timeout}
+    context = {'first_map': address, 'cache_timeout': cache_timeout, 'errors':errors}
     return render(request, 'twitter_status_map/index.html', context)
